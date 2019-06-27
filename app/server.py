9ae7a89d99ee547bc78fed34c7c578e076952af8
Siddhant Dubey <sid12.dubey@gmail.com>
2019-06-27 17:40:59 -0400

    changed the model

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=104agqmMOT6Lvhw1SISZPhl2Rma0cLFOB'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-CsGyLrDzJCO9K-45htU0F8ZTnmxpoN1'
 export_file_name = 'export.pkl'
 
 classes = ['real', 'fake']
